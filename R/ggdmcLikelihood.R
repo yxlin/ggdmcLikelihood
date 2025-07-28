5b0a118ea1011bdf82fb28944be2137ded61a3fa
Yi-Shin Lin <yishinlin001@gmail.com>
2025-07-28 08:30:34 +0800

    correct the typo initi(a)lisation

--- R/ggdmcLikelihood.R
+++ R/ggdmcLikelihood.R
@@ -4,7 +4,7 @@
 #' models, including the Decision Diffusion Model, is supported. The package 
 #' enables rapid evaluation of likelihood functions for both 
 #' single- and multi-subject models across trial-level data. It also offers 
-#' fast initilisation of starting parameters for genetic sampling with many 
+#' fast initialisation of starting parameters for genetic sampling with many 
 #' Markov chains, facilitating estimation in complex models typically found 
 #' in experimental psychology and behavioural science. These optimisations 
 #' help reduce computational overhead in large-scale model fitting tasks.
